cce39884ce8ccf350561732e5c2654c8e2c5a5ca
max <github.com@maxos.email>
2022-04-27 13:00:00 +0800

    Initial Commit

---
+++ Shared/flashApp.swift
@@ -0,0 +1,17 @@
+//
+//  flashApp.swift
+//  Shared
+//
+//  Created by Max Zhang on 2022/4/27.
+//
+
+import SwiftUI
+
+@main
+struct flashApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
